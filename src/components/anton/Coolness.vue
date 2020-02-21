f21bbcfb2ca765feeec47dbefa64d87c867cf034
Anton Purver <270025112@acgedu.com>
2020-02-21 18:17:29 +1300

    Antons Commit

---
+++ src/components/anton/Coolness.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>WHAT A VIEW!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'coolness'
+};
+</script>
+
+<style scoped>
+
+.container{
+  background: url('https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+  height: 800px;
+  background-position: bottom;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sans-serif;
+}
+h1{
+  font-weight: 200;
+  font-size: 4rem;
+  background-color: rgba(241, 193, 138, 0.1);
+  padding: 20px;
+  color: rgba(255, 255, 255, 0.493);
+}
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,18 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import Coolness from './components/anton/Coolness'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-const routes = [];
+const routes = [{
+  name: "Coolness",
+  path: "/coolness",
+  component: Coolness
+}];
 
 const router = new VueRouter({
   routes,
   mode: 'history'
-  })
+})
 
 new Vue({
   render: h => h(App),
